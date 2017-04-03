3f42d06f47e37dfedbefa559ae7812c3b16aede6
Ram <ramn0711@gmail.com>
2017-04-03 13:12:36 -0400

    Added updates to the interface and service.

--- WCF REST API/Services/IMyService.cs
+++ WCF REST API/Services/IMyService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Web.UI;
+using System.ServiceModel.Web;
 
 namespace WCF_REST_API.Services
 {
@@ -12,6 +14,15 @@ namespace WCF_REST_API.Services
     public interface IMyService
     {
         [OperationContract]
-        void DoWork();
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoWork")]
+        String DoWork();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoSquare")]
+        int DoSquare(int value);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoAddValues")]
+        int DoAddValues(AddValues addValues);
     }
 }
--- WCF REST API/Services/MyService.svc.cs
+++ WCF REST API/Services/MyService.svc.cs
@@ -3,16 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Activation;
 using System.Text;
 
 namespace WCF_REST_API.Services
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select MyService.svc or MyService.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class MyService : IMyService
     {
-        public void DoWork()
+        public int DoAddValues(AddValues addValues)
         {
+            throw new NotImplementedException();
+        }
+
+        public int DoSquare(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public String DoWork()
+        {
+            return "Hello WCF Rest Service!";
+        }
+    }
+
+    [DataContract]
+    public class AddValues
+    {
+        [DataMember]
+        public int Value1 { get; set; }
+        [DataMember]
+        public int Value2 { get; set; }
+
+        public AddValues()
+        {
+            Value1 = 0;
+            Value2 = 0;
         }
     }
 }
